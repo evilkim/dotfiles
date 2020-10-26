47297d08f04ca0b287a465c824c3f563e891e06b
Kim Taylor <kim.taylor@bnymellon.com>
2020-10-26 20:53:55 +0100

    Export builtin COLUMNS var to make it more avaialble. Explicitly (re)export LOGNAME, USER. Move shell var TIMEFORMAT to vars.sh.

--- etc/env.sh
+++ etc/env.sh
@@ -1,14 +1,19 @@
-# Miscellanous variables
-
-TIMEFORMAT="time: %0lR"		# unset to revert to default
-
-# Set these near universal variables as needed.
-: ${USER:=$(id -un)}		# Expected by BSD programs
-: ${LOGNAME:=$(logname)}	# Expected by SystemV programs
-
+# Miscellanous environment variables
+#
 # SEE ALSO
 # * https://unix.stackexchange.com/questions/76354/\
-#   who-sets-user-and-username-environment-variables/76356
+    # who-sets-user-and-username-environment-variables/76356
 # * id(1)
 # * logname(1)
+# * path.sh
 # * whoami(1)
+
+# Export these near universal variables as needed.
+: ${LOGNAME:=$(logname)}	# Expected by SystemV programs
+: ${USER:=$(id -un)}		# Expected by BSD programs
+export LOGNAME USER
+
+# Pass COLUMNS thru to commands that want to know the terminal width, e.g:
+# https://github.com/evilkim/bashisms/git_sdiff
+# N.B: As needed, resizing the terminal may initialize COLUMNS.
+export COLUMNS
