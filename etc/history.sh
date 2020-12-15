c7a7faabbfb4a36adabf36243a2559c9d19b10b9
Kim Taylor <kim.taylor@bnymellon.com>
2020-12-15 17:16:01 -0500

    etc/history.sh - Configure HISTTIMEFORMAT, HISTCONTROL, and HISTIGNORE.

--- etc/history.sh
+++ etc/history.sh
@@ -1,3 +1,10 @@
 HISTFILE="${HOME}/.bash_history.${OSTYPE}"
 HISTSIZE=2000
 HISTFILESIZE=${HISTSIZE}
+HISTTIMEFORMAT="%h %d %H:%M:%S "
+HISTCONTROL="ignorespace:ignoredups"
+HISTIGNORE="history"
+
+# SEE ALSO
+# * ~/etc/prompt.sh
+# * https://www.shellhacks.com/tune-command-line-history-bash
